1536267748b4107c6a3a97393889afaa00839305
ErikahMya <enzozobrado@zrc.pshs.edu.ph>
2026-09-17 16:59:16 -0700

    hi sir ila nako ni :)

--- q1/sg6/real-life-relationship-mapping/main.py
+++ q1/sg6/real-life-relationship-mapping/main.py
@@ -1,1 +1,15 @@
 # Write a short Python code snippet showing a Course adding a Student object to a list. 
+class Student:
+    def __init__(self, name, student_id):
+        self.__name = name
+        self.__student_id = __student_id
+    def get_info(self):
+        return self.__name
+
+class Course:
+    def __init__(self,course_name):
+        self.__course_name = course_name
+        self.__students = []
+
+    def add_student(self, student):
+        self.__studdnts.append(student) 
